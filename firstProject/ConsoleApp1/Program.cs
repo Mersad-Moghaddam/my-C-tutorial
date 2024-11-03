3613eb2b72b5cf2189cc5be50dfe6a395d68061d
Mersad <nickmersad81@gmail.com>
2024-11-03 09:47:32 +0330

    Hello C#

---
+++ firstProject/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine()!;
+Console.WriteLine("How old are you?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("What is your job?");
+string job = Console.ReadLine()!;
+Console.WriteLine("Are you a student?");
+bool isStudent = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("How many years of coding experience do you have?");
+int codingAge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Do you love C#?");
+bool isLoveCSharp = Convert.ToBoolean(Console.ReadLine());
+
+
+string love;
+string student;
+if (isLoveCSharp)
+{
+    love = "I love C#";
+}
+else
+{
+    love = "I don't love C#";
+}
+if (isStudent)
+{
+    student = "I am a student";
+
+}
+else
+{
+    student = "I am not a student";
+}
+Console.WriteLine($"Hello, my name is {name}, I am {age} years old {job},\n {love},and {student} also i have {codingAge} years of coding experience");
+
+

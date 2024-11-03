ab7806db163ab5bb8926a9ba7f6c5edda450fff5
Mersad <nickmersad81@gmail.com>
2024-11-03 10:00:10 +0330

    warmUp

--- firstProject/ConsoleApp1/Program.cs
+++ firstProject/ConsoleApp1/Program.cs
@@ -31,6 +31,19 @@ else
 {
     student = "I am not a student";
 }
+
+
+
+Console.WriteLine("Enter your favorite car names (separated by commas):");
+string input = Console.ReadLine()!;
+string[] cars = input.Split(',');
+
+
 Console.WriteLine($"Hello, my name is {name}, I am {age} years old {job},\n {love},and {student} also i have {codingAge} years of coding experience");
+Console.WriteLine("My favorite cars are:");
+foreach (var car in cars)
+{
+    Console.WriteLine(car);
+}
 
 
